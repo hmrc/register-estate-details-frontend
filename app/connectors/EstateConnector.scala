9f458a5806bfe5bae1bf93089b2b74682e81f153
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2020-11-16 10:24:21 +0000

    TRUS-3337: Upgrade play to 2.7.5

--- app/connectors/EstateConnector.scala
+++ app/connectors/EstateConnector.scala
@@ -22,7 +22,7 @@ import models.EstateDetails
 import play.api.libs.json.{JsString, JsSuccess, JsValue}
 import uk.gov.hmrc.http.HttpReads.Implicits._
 import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
-import uk.gov.hmrc.play.bootstrap.http.HttpClient
+import uk.gov.hmrc.http.HttpClient
 
 import scala.concurrent.{ExecutionContext, Future}
 
