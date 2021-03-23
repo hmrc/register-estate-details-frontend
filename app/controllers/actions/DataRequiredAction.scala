a8dd6d7e97ad30c26794b7fb38f23e2772d3fe46
“JoshuaH01” <34240122+JoshuaH01@users.noreply.github.com>
2021-03-23 19:07:26 +0000

    PlayUpgrade| To 4.1

--- app/controllers/actions/DataRequiredAction.scala
+++ app/controllers/actions/DataRequiredAction.scala
@@ -23,7 +23,7 @@ import play.api.Logging
 import play.api.mvc.Results.Redirect
 import play.api.mvc.{ActionRefiner, Result}
 import uk.gov.hmrc.http.HeaderCarrier
-import uk.gov.hmrc.play.HeaderCarrierConverter
+import uk.gov.hmrc.play.http.HeaderCarrierConverter
 import utils.Session
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -32,7 +32,7 @@ class DataRequiredActionImpl @Inject()(implicit val executionContext: ExecutionC
 
   override protected def refine[A](request: OptionalDataRequest[A]): Future[Either[Result, DataRequest[A]]] = {
 
-    implicit val hc : HeaderCarrier = HeaderCarrierConverter.fromHeadersAndSession(request.headers, Some(request.session))
+    implicit val hc : HeaderCarrier = HeaderCarrierConverter.fromRequestAndSession(request, request.session)
 
     request.userAnswers match {
       case None =>
