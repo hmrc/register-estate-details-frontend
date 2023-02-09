e745a5e1a454201bc8473d5380ae7072e9cee299
Victor Osipov <osipovvictor1994@gmail.com>
2023-02-09 14:41:35 +0000

    [DDCE-3342] - Update deps, + sbt, + plugins (#62)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,44 +1,31 @@
 import sbt._
+import play.core.PlayVersion
 
 object AppDependencies {
-  import play.core.PlayVersion
 
-  val compile = Seq(
+  private val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "0.73.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "1.1.0-play-28",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.9.0-play-28",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % "7.8.0"
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "0.74.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "6.3.0-play-28",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.12.0-play-28",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % "7.13.0"
   )
 
-  val test: Seq[ModuleID] = Seq(
-    "org.scalatest"               %% "scalatest"                  % "3.2.12",
+  private val test: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc"                %% "bootstrap-test-play-28"      % "7.13.0",
+    "org.scalatest"               %% "scalatest"                  % "3.2.15",
     "org.scalatestplus.play"      %% "scalatestplus-play"         % "5.1.0",
-    "org.pegdown"                 %  "pegdown"                    % "1.6.0",
-    "org.jsoup"                   %  "jsoup"                      % "1.15.1",
+    "org.jsoup"                   %  "jsoup"                      % "1.15.3",
     "com.typesafe.play"           %% "play-test"                  % PlayVersion.current,
-    "org.mockito"                 %  "mockito-all"                % "1.10.19",
-    "org.mockito"                 %  "mockito-core"               % "4.6.1",
-    "org.scalatestplus"           %% "mockito-3-12"               % "3.2.10.0",
-    "org.scalacheck"              %% "scalacheck"                 % "1.16.0",
+    "org.scalatestplus"           %% "mockito-4-6"                % "3.2.15.0",
+    "org.scalacheck"              %% "scalacheck"                 % "1.17.0",
     "org.scalatestplus"           %% "scalatestplus-scalacheck"   % "3.1.0.0-RC2",
     "com.github.tomakehurst"      % "wiremock-standalone"         % "2.27.2",
     "wolfendale"                  %% "scalacheck-gen-regexp"      % "0.1.2",
     "com.vladsch.flexmark"        % "flexmark-all"                % "0.62.2",
-    "uk.gov.hmrc.mongo"           %% "hmrc-mongo-test-play-28"    % "0.73.0"
+    "uk.gov.hmrc.mongo"           %% "hmrc-mongo-test-play-28"    % "0.74.0"
   ).map(_ % "test, it")
 
   def apply(): Seq[ModuleID] = compile ++ test
 
-  val akkaVersion = "2.6.10"
-  val akkaHttpVersion = "10.1.12"
-
-  val overrides = Seq(
-    "com.typesafe.akka" %% "akka-stream_2.12"     % akkaVersion,
-    "com.typesafe.akka" %% "akka-protobuf_2.12"   % akkaVersion,
-    "com.typesafe.akka" %% "akka-slf4j_2.12"      % akkaVersion,
-    "com.typesafe.akka" %% "akka-actor_2.12"      % akkaVersion,
-    "com.typesafe.akka" %% "akka-http-core_2.12"  % akkaHttpVersion,
-    "commons-codec"     %  "commons-codec"        % "1.12"
-  )
 }
--- test/controllers/EstateNameControllerSpec.scala
+++ test/controllers/EstateNameControllerSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 HM Revenue & Customs
+ * Copyright 2023 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@ import base.SpecBase
 import connectors.{EstateConnector, EstatesStoreConnector}
 import forms.EstateNameFormProvider
 import models.NormalMode
-import org.mockito.Matchers._
+import org.mockito.ArgumentMatchers._
 import org.mockito.Mockito._
 import org.scalatestplus.mockito.MockitoSugar
 import pages.EstateNamePage
