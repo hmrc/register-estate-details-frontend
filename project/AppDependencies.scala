4993f8215cd6eb346dd4f979b502a3116466ec8c
Mark Beresford <mark.beresford@digital.hmrc.gov.uk>
2024-07-22 13:59:39 +0100

    DDCE-5564: Update to play hmrc V9.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   private val compile = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"             % mongoVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"     % "8.5.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"     % "9.11.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "2.0.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"     % bootstrapVersion
   )
@@ -16,7 +16,7 @@ object AppDependencies {
     "uk.gov.hmrc"                 %% "bootstrap-test-play-30"     % bootstrapVersion,
     "uk.gov.hmrc.mongo"           %% "hmrc-mongo-test-play-30"    % mongoVersion,
     "org.scalatestplus"           %% "scalacheck-1-17"            % "3.2.18.0",
-    "org.jsoup"                   %  "jsoup"                      % "1.17.2",
+    "org.jsoup"                   %  "jsoup"                      % "1.18.1",
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
