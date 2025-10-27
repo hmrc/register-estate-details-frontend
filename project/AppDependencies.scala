79b7a39f8ac17f5b43ad68a82d53f301a93ac97d
mithra-balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2025-10-27 10:25:03 +0000

    DDCE-6820 - gov uk rebranding

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.19.0"
+  private val bootstrapVersion = "10.2.0"
   private val mongoVersion = "2.10.0"
 
   private val compile = Seq(
