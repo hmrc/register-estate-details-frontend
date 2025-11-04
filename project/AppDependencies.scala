44994679ab419b9a937216159cbac6a9e61ea1ce
matthew-gibson-18 <175828706+matthew-gibson-18@users.noreply.github.com>
2025-11-04 08:44:01 +0000

    DDCE-7579 Update repository.yaml to Match Standardized Format

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.2.0"
+  private val bootstrapVersion = "10.4.0"
   private val mongoVersion = "2.10.0"
 
   private val compile = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"             % mongoVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"     % "12.18.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"     % "12.20.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"     % bootstrapVersion
   )
@@ -16,7 +16,6 @@ object AppDependencies {
     "uk.gov.hmrc"                 %% "bootstrap-test-play-30"     % bootstrapVersion,
     "uk.gov.hmrc.mongo"           %% "hmrc-mongo-test-play-30"    % mongoVersion,
     "org.scalatestplus"           %% "scalacheck-1-18"            % "3.2.19.0",
-    "org.jsoup"                   %  "jsoup"                      % "1.21.1",
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
